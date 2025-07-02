eb6c83373090a453dd86a0eb04b3ad8256edd394
abc <clvlajrrhtlvek@gmail.com>
2025-07-02 23:33:25 +0900

    initial

---
+++ src/main/java/api/docq/DocQApplication.java
@@ -0,0 +1,13 @@
+package api.docq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocQApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocQApplication.class, args);
+    }
+
+}
