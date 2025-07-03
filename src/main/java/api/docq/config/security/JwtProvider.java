a772d633651be604028137f1cba83c449de2b9bc
jiwonclvl <clvlajrrhtlvek@gmail.com>
2025-07-03 19:58:49 +0900

    feat(DQ-10): apply jwt token

---
+++ src/main/java/api/docq/config/security/JwtProvider.java
@@ -0,0 +1,52 @@
+package api.docq.config.security;
+
+import api.docq.domain.user.enums.UserRole;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+
+    //todo: 테스트를 위해 12시간으로 설정 추후 짧게 설정하기
+    private static final long ACCESS_TOKEN_EXPIRATION_TIME = 12 * 60 * 60 * 1000L; //12시간
+
+    //todo: 추후 refresh 토큰 추가하기
+    private static final long REFRESH_TOKEN_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000L; //일주일
+
+    @PostConstruct
+    public void init() {
+        byte[] decodeBytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(decodeBytes);
+    }
+
+    public String createAccessToken(Long userId, String email, UserRole role) {
+        return Jwts.builder()
+                .claim("id", userId)
+                .claim("email", email)
+                .claim("role", role)
+                .expiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION_TIME))
+                .issuedAt(new Date())
+                .signWith(key)
+                .compact();
+    }
+
+    public Claims extractToken(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+}
