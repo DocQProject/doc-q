90caaf6008ae559f993d2d8373ba901273e3d639
seungminJ <seungmin103@gmail.com>
2025-07-05 22:05:17 +0900

    feat(DQ-14): Add validation for writing a post

--- src/main/java/api/docq/domain/post/dto/request/PostRequest.java
+++ src/main/java/api/docq/domain/post/dto/request/PostRequest.java
@@ -1,14 +1,17 @@
 package api.docq.domain.post.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class PostRequest {
 
+    @NotBlank(message = "제목을 입력하세요")
     private final String title;
 
     private final String author;
 
+    @NotBlank(message = "내용을 입력하세요")
     private final String content;
 
     public PostRequest(String title, String author, String content) {
